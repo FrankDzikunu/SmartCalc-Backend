5a0b5dc0f15530c1084abed1e3f6c271e6ea6660
Frank Dzikunu <frankdzikunu@gmail.com>
2026-01-12 16:13:20 +0000

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
+# users/admin.py
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "must_change_password")
+    list_filter = ("must_change_password",)
